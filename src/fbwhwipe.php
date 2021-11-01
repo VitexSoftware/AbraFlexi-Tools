130ed56763fe628247052aa0d2e109333f6c8f0b
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-11-01 22:56:47 +0100

    locad config file only if exits

--- src/fbwhwipe.php
+++ src/fbwhwipe.php
@@ -20,22 +20,18 @@ $config_file = $argc > 1 ? $argv[1] : '/etc/abraflexi/client.json';
 
 if (file_exists($config_file)) {
     \Ease\Shared::singleton()->loadConfig($config_file, true);
-
-    $hooker = new \AbraFlexi\Hooks();
-    $hooks = $hooker->getFlexiData();
-    if (!isset($hooks['message']) && is_array($hooks) && !empty(current($hooks)) && count(current($hooks))) {
-        foreach ($hooks as $hookinfo) {
-            if ($hooker->unregister($hookinfo['id'])) {
-                $hooker->addStatusMessage(sprintf(_('Hook %s was unregistered'),
-                                $hookinfo['url']), 'success');
-            } else {
-                $hooker->addStatusMessage(sprintf(_('Hook %s was not unregistered'),
-                                $hookinfo['url']), 'warning');
-            }
-        }
-    }
-} else {
-    \Ease\Shared::instanced()->addStatusMessage(_('Cannot read %s'),
-            $config_file, 'error');
 }
 
+$hooker = new \AbraFlexi\Hooks();
+$hooks = $hooker->getFlexiData();
+if (!isset($hooks['message']) && is_array($hooks) && !empty(current($hooks)) && count(current($hooks))) {
+    foreach ($hooks as $hookinfo) {
+        if ($hooker->unregister($hookinfo['id'])) {
+            $hooker->addStatusMessage(sprintf(_('Hook %s was unregistered'),
+                            $hookinfo['url']), 'success');
+        } else {
+            $hooker->addStatusMessage(sprintf(_('Hook %s was not unregistered'),
+                            $hookinfo['url']), 'warning');
+        }
+    }
+}
